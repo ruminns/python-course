67c3d172cb8ad471fb74a0f0bd4658830ee30d87
ruminns <da_afanasieva@mail.ru>
2023-02-13 01:02:55 +0300

    Update first.py

---
+++ 2 lesson/first.py
@@ -0,0 +1,22 @@
+print (1 == 2)
+print (1 > 2)
+print (1 < 2)
+print (1 != 2)
+print (1 >= 2)
+print (1 <= 2)
+
+print (1+1 == 2 or 1*1 == 2)
+print (1+1 == 2 and 1*1 == 1)
+print (1+1 == 2 and not 1*1 == 2)
+
+print (3**2)
+print (10/3)
+print (10//3)
+print (10%3)
+
+a = 13
+a+=13
+print (a)
+
+print ('привет' in 'привет мир')
+print ('пока' not in 'привет мир')
